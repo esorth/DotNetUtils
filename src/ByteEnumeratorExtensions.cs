792333944faa43f79436fc7905c61f245d863f01
Eric Orth <esorth@gmail.com>
2024-05-10 20:35:57 -0400

    Add extensions to read various ints from a byte enumerator.

---
+++ src/ByteEnumeratorExtensions.cs
@@ -0,0 +1,91 @@
+﻿// .Net Utils - Misc utility classes/functions for use in .Net libraries.
+// Written in 2024 by Eric Orth
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+// You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see http://creativecommons.org/publicdomain/zero/1.0/.
+
+using System.Buffers.Binary;
+using System.Collections.Immutable;
+
+namespace DotNetUtils
+{
+    public static class ByteEnumeratorExtensions
+    {
+        public static byte ReadByte(this IEnumerator<byte> e)
+        {
+            if (e is null)
+            {
+                throw new ArgumentNullException(nameof(e));
+            }
+
+            if (!e.MoveNext())
+            {
+                throw new ArgumentException("Not enough data.", nameof(e));
+            }
+            return e.Current;
+        }
+
+        public static IImmutableList<byte> ReadBytes(this IEnumerator<byte> e, int numBytes)
+        {
+            if (numBytes < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numBytes), "Must be at least 0.");
+            }
+
+            ImmutableArray<byte>.Builder builder = ImmutableArray.CreateBuilder<byte>(numBytes);
+            for (int i = 0; i < numBytes; i++)
+            {
+                builder.Add(e.ReadByte());
+            }
+            return builder.ToImmutable();
+        }
+
+        public static UInt16 ReadLittleEndianUInt16(this IEnumerator<byte> e)
+        {
+            IImmutableList<byte> bytes = e.ReadBytes(2);
+            return BinaryPrimitives.ReadUInt16LittleEndian(bytes.ToImmutableSpan(..));
+        }
+
+        public static UInt16 ReadBigEndianUInt16(this IEnumerator<byte> e)
+        {
+            IImmutableList<byte> bytes = e.ReadBytes(2);
+            return BinaryPrimitives.ReadUInt16BigEndian(bytes.ToImmutableSpan(..));
+        }
+
+        public static Int16 ReadLittleEndianInt16(this IEnumerator<byte> e)
+        {
+            IImmutableList<byte> bytes = e.ReadBytes(2);
+            return BinaryPrimitives.ReadInt16LittleEndian(bytes.ToImmutableSpan(..));
+        }
+
+        public static Int16 ReadBigEndianInt16(this IEnumerator<byte> e)
+        {
+            IImmutableList<byte> bytes = e.ReadBytes(2);
+            return BinaryPrimitives.ReadInt16BigEndian(bytes.ToImmutableSpan(..));
+        }
+
+        public static UInt32 ReadLittleEndianUInt32(this IEnumerator<byte> e)
+        {
+            IImmutableList<byte> bytes = e.ReadBytes(4);
+            return BinaryPrimitives.ReadUInt32LittleEndian(bytes.ToImmutableSpan(..));
+        }
+
+        public static UInt32 ReadBigEndianUInt32(this IEnumerator<byte> e)
+        {
+            IImmutableList<byte> bytes = e.ReadBytes(4);
+            return BinaryPrimitives.ReadUInt32BigEndian(bytes.ToImmutableSpan(..));
+        }
+
+        public static Int32 ReadLittleEndianInt32(this IEnumerator<byte> e)
+        {
+            IImmutableList<byte> bytes = e.ReadBytes(4);
+            return BinaryPrimitives.ReadInt32LittleEndian(bytes.ToImmutableSpan(..));
+        }
+
+        public static Int32 ReadBigEndianInt32(this IEnumerator<byte> e)
+        {
+            IImmutableList<byte> bytes = e.ReadBytes(4);
+            return BinaryPrimitives.ReadInt32BigEndian(bytes.ToImmutableSpan(..));
+        }
+    }
+}
---
+++ tests/ByteEnumeratorExtensionsTests.cs
@@ -0,0 +1,139 @@
+﻿// .Net Utils - Misc utility classes/functions for use in .Net libraries.
+// Written in 2024 by Eric Orth
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+// You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see http://creativecommons.org/publicdomain/zero/1.0/.
+
+namespace DotNetUtils.Tests
+{
+    [TestClass()]
+    public class ByteEnumeratorExtensionsTests
+    {
+        [TestMethod()]
+        public void ReadByte()
+        {
+            Assert.AreEqual(
+                1, (new byte[] { 1 } as IEnumerable<byte>).GetEnumerator().ReadByte());
+            Assert.AreEqual(
+                1, (new byte[] { 1, 2 } as IEnumerable<byte>).GetEnumerator().ReadByte());
+        }
+
+        [TestMethod()]
+        public void ReadBytes()
+        {
+            CollectionAssert.AreEqual(
+                new byte[] { 1, 2, 3, 4 },
+                (new byte[] { 1, 2, 3, 4 } as IEnumerable<byte>).GetEnumerator().ReadBytes(4).ToArray());
+            CollectionAssert.AreEqual(
+                new byte[] { 1, 2, 3, 4 },
+                (new byte[] { 1, 2, 3, 4, 5, 6 } as IEnumerable<byte>).GetEnumerator().ReadBytes(4).ToArray());
+            CollectionAssert.AreEqual(
+                Array.Empty<byte>(),
+                (new byte[] { 1, 2, 3, 4, 5, 6 } as IEnumerable<byte>).GetEnumerator().ReadBytes(0).ToArray());
+        }
+
+        [TestMethod()]
+        public void ReadLittleEndianUInt16()
+        {
+            Assert.AreEqual(13330, (new byte[] { 0x12, 0x34 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianUInt16());
+            Assert.AreEqual(13330, (new byte[] { 0x12, 0x34, 0x56 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianUInt16());
+            Assert.AreEqual(56574, (new byte[] { 0xFE, 0xDC } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianUInt16());
+            Assert.AreEqual(56574, (new byte[] { 0xFE, 0xDC, 0xBA } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianUInt16());
+        }
+
+        [TestMethod()]
+        public void ReadBigEndianUInt16()
+        {
+            Assert.AreEqual(4660, (new byte[] { 0x12, 0x34 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianUInt16());
+            Assert.AreEqual(4660, (new byte[] { 0x12, 0x34, 0x56 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianUInt16());
+            Assert.AreEqual(65244, (new byte[] { 0xFE, 0xDC } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianUInt16());
+            Assert.AreEqual(65244, (new byte[] { 0xFE, 0xDC, 0xBA } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianUInt16());
+        }
+
+        [TestMethod()]
+        public void ReadLittleEndianInt16()
+        {
+            Assert.AreEqual(13330, (new byte[] { 0x12, 0x34 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianInt16());
+            Assert.AreEqual(13330, (new byte[] { 0x12, 0x34, 0x56 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianInt16());
+            Assert.AreEqual(-8962, (new byte[] { 0xFE, 0xDC } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianInt16());
+            Assert.AreEqual(-8962, (new byte[] { 0xFE, 0xDC, 0xBA } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianInt16());
+        }
+
+        [TestMethod()]
+        public void ReadBigEndianInt16()
+        {
+            Assert.AreEqual(4660, (new byte[] { 0x12, 0x34 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianInt16());
+            Assert.AreEqual(4660, (new byte[] { 0x12, 0x34, 0x56 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianInt16());
+            Assert.AreEqual(-292, (new byte[] { 0xFE, 0xDC } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianInt16());
+            Assert.AreEqual(-292, (new byte[] { 0xFE, 0xDC, 0xBA } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianInt16());
+        }
+
+        [TestMethod()]
+        public void ReadLittleEndianUInt32()
+        {
+            Assert.AreEqual(2018915346u, (new byte[] { 0x12, 0x34, 0x56, 0x78 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianUInt32());
+            Assert.AreEqual(2018915346u, (new byte[] { 0x12, 0x34, 0x56, 0x78, 0x9A } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianUInt32());
+            Assert.AreEqual(2562383102u, (new byte[] { 0xFE, 0xDC, 0xBA, 0x98 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianUInt32());
+            Assert.AreEqual(2562383102u, (new byte[] { 0xFE, 0xDC, 0xBA, 0x98, 0x76 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianUInt32());
+        }
+
+        [TestMethod()]
+        public void ReadBigEndianUInt32()
+        {
+            Assert.AreEqual(305419896u, (new byte[] { 0x12, 0x34, 0x56, 0x78 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianUInt32());
+            Assert.AreEqual(305419896u, (new byte[] { 0x12, 0x34, 0x56, 0x78, 0x9A } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianUInt32());
+            Assert.AreEqual(4275878552u, (new byte[] { 0xFE, 0xDC, 0xBA, 0x98 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianUInt32());
+            Assert.AreEqual(4275878552u, (new byte[] { 0xFE, 0xDC, 0xBA, 0x98, 0x76 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianUInt32());
+        }
+
+        [TestMethod()]
+        public void ReadLittleEndianInt32()
+        {
+            Assert.AreEqual(2018915346, (new byte[] { 0x12, 0x34, 0x56, 0x78 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianInt32());
+            Assert.AreEqual(2018915346, (new byte[] { 0x12, 0x34, 0x56, 0x78, 0x9A } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianInt32());
+            Assert.AreEqual(-1732584194, (new byte[] { 0xFE, 0xDC, 0xBA, 0x98 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianInt32());
+            Assert.AreEqual(-1732584194, (new byte[] { 0xFE, 0xDC, 0xBA, 0x98, 0x76 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadLittleEndianInt32());
+        }
+
+        [TestMethod()]
+        public void ReadBigEndianInt32()
+        {
+            Assert.AreEqual(305419896, (new byte[] { 0x12, 0x34, 0x56, 0x78 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianInt32());
+            Assert.AreEqual(305419896, (new byte[] { 0x12, 0x34, 0x56, 0x78, 0x9A } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianInt32());
+            Assert.AreEqual(-19088744, (new byte[] { 0xFE, 0xDC, 0xBA, 0x98 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianInt32());
+            Assert.AreEqual(-19088744, (new byte[] { 0xFE, 0xDC, 0xBA, 0x98, 0x76 } as IEnumerable<byte>)
+                                       .GetEnumerator().ReadBigEndianInt32());
+        }
+    }
+}
